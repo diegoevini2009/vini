559d07ad3278260d7853bcf3348f70b85e238ee4
diegoevini2009 <vinicius.lenartowicz@escola.pr.gov.br>
2026-06-02 14:12:49 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const botao = document.getElementById("btnMensagem");
+
+botao.addEventListener("click", () => {
+    alert(
+        "O campo e a cidade dependem um do outro. Juntos, constroem um futuro mais sustentável e próspero!"
+    );
+});
